ec065de770e88ece26f4bbf30f2275287dec15b7
Anastasiia <144330798+NastyaDudk@users.noreply.github.com>
2024-04-16 13:43:17 +0400

    okey

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,17 @@
 import { Suspense, lazy } from "react";
-import { BrowserRouter as Router, Route, Routes, } from "react-router-dom";
-import MoviesPage from "./pages/MoviesPage/MoviesPage.jsx"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import MoviesPage from "./pages/MoviesPage/MoviesPage.jsx";
 import Navigation from "./components/Navigation/Navigation.jsx";
-import MovieCast from "./components/MovieCast/MovieCast.jsx"
+import MovieCast from "./components/MovieCast/MovieCast.jsx";
 const HomePage = lazy(() => import("./pages/HomePage/HomePage.jsx"));
-const MovieDetailsPage = lazy(() =>
-  import("./pages/MovieDetailsPage/MovieDetailsPage.jsx")
-);
-const NotFoundPage = lazy(() =>
-  import("./pages/NotFoundPage/NotFoundPage.jsx")
-);
-
-const MovieReviews = lazy(() =>
-  import("./components/MovieReviews/MovieReviews.jsx")
-);
+const MovieDetailsPage = lazy(() => import("./pages/MovieDetailsPage/MovieDetailsPage.jsx"));
+const NotFoundPage = lazy(() => import("./pages/NotFoundPage/NotFoundPage.jsx"));
+const MovieReviews = lazy(() => import("./components/MovieReviews/MovieReviews.jsx"));
 import Loader from "./components/Loader/Loader.jsx";
-
-import css from "./App.module.css";  
+import css from "./App.module.css";
 
 const App = () => {
   return (
- 
     <>
       <Navigation />
       <div className={css.container}>
@@ -28,16 +19,15 @@ const App = () => {
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/movies" element={<MoviesPage />} />
-             <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
-            <Route path="/movies/:movieId/cast" element={<MovieCast />} />
-            <Route path="/movies/:movieId/reviews" element={<MovieReviews />} />
+            <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
+              <Route path="cast" element={<MovieCast />} />
+              <Route path="reviews" element={<MovieReviews />} />
             </Route>
             <Route path="*" element={<NotFoundPage />} />
           </Routes>
         </Suspense>
       </div>
-      </>
-     
+    </>
   );
 };
 
--- src/components/MovieCast/MovieCast.jsx
+++ src/components/MovieCast/MovieCast.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect, useRef } from "react";
 import { useParams } from "react-router-dom";
-import {fetchMovieCast} from "../../api.js";
-import css from './MovieCast.module.css'
+import { fetchMovieCast } from "../../api.js";
+import css from './MovieCast.module.css';
 import Loader from "../Loader/Loader.jsx";
 import ErrorMessage from "../ErrorMessage/ErrorMessage.jsx";
 
@@ -12,31 +12,35 @@ const MovieCast = () => {
   const { movieId } = useParams();
   const castRef = useRef();
 
- useEffect(() => {
-  const getCast = async () => {
-    try {
-      setError(false);
-      setLoading(true);
-      const cast = await fetchMovieCast(movieId);
-      setCast(cast);
-    } catch (error) {
-      setError(error.message);
-    } finally {
-      setLoading(false);
-    }
-  };
-  getCast();
+  useEffect(() => {
+    const getCast = async () => {
+      try {
+        setError(null);
+        setLoading(true);
+        const cast = await fetchMovieCast(movieId);
+        setCast(cast);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-  
-    const tID = setTimeout(() => {
-  if (castRef.current) {
-    window.scrollTo({
-      top: castRef.current.offsetTop - 130,
-      behavior: 'smooth',
-    });
-  }
+    getCast();
+
+    const timeoutId = setTimeout(() => {
+      if (castRef.current) {
+        window.scrollTo({
+          top: castRef.current.offsetTop - 130,
+          behavior: 'smooth',
+        });
+      }
     }, 500);
-    }, [movieId]);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [movieId]);
 
   return (
     <>
@@ -44,29 +48,27 @@ const MovieCast = () => {
       {error && <ErrorMessage error={error} />}
       {cast && (
         <ul className={css.castListWrapper} ref={castRef}>
-          {cast.map(({ name, character, image }) => {
-            return (
-              <li key={name}>
-                <ul className={css.infoList}>
-                  <li className={css.infoImageWrapper}>
-                    <img
-                      className={css.infoImage}
-                      src={image}
-                      alt={name}
-                      width="250px"
-                      height="350px"
-                    />
-                  </li>
-                  <li className={css.infoName}>
-                    <p>{name}</p>
-                  </li>
-                  <li>
-                    <p>as {character}</p>
-                  </li>
-                </ul>
-              </li>
-            );
-          })}
+          {cast.map(({ name, character, image }) => (
+            <li key={name}>
+              <ul className={css.infoList}>
+                <li className={css.infoImageWrapper}>
+                  <img
+                    className={css.infoImage}
+                    src={image}
+                    alt={name}
+                    width="250px"
+                    height="350px"
+                  />
+                </li>
+                <li className={css.infoName}>
+                  <p>{name}</p>
+                </li>
+                <li>
+                  <p>as {character}</p>
+                </li>
+              </ul>
+            </li>
+          ))}
         </ul>
       )}
       {cast && !cast.length && <p>No information about cast</p>}
